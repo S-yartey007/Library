0ca27efbb68f0250b8089621eb9a90aa7120d43b
Emmanuel Yartey <supremeconquerorx@gmail.com>
2024-09-20 15:28:21 +0000

    class implementation of library

--- index.js
+++ index.js
@@ -11,65 +11,59 @@
 //Add a button on each book's display to change the read status
 
 //Create an array to store books
-const main = document.querySelector(".main")
-const book_section = document.querySelector(".books")
-const dialog = document.querySelector("#m-dialog");
-const confirmBtn = document.querySelector("#confirmBtn");
-const cancelBtn = document.querySelector("#cancelBtn");
-const author = document.querySelector("#author");
-const title = document.querySelector("#title");
-const pages = document.querySelector("#pages");
-const isRead = document.querySelector("#isRead");
-const newBook = document.querySelector(".new-book");
-const status_dialog = document.querySelector("#select-status")
+class State {
+    library = [
+        {
+            title : "harry pooter",
+            author: "micheal",
+            pages: 300,
+            isRead: false
+        },
+        {
+            title : "journey to the west",
+            author: "choas incarnate",
+            pages: 300,
+            isRead: false
+        }, 
+        {
+            title : "kunfu panda",
+            author: "void general",
+            pages: 300,
+            isRead: false
+        },
+        {
+            title : "diablo",
+            author: "fantastic",
+            pages: 300,
+            isRead: false
+        }
+    ];
+    
+    constructor() {
+        this.main = document.querySelector(".main")
+        this.book_section = document.querySelector(".books")
+        this.dialog = document.querySelector("#m-dialog");
+        this.confirmBtn = document.querySelector("#confirmBtn");
+        this.cancelBtn = document.querySelector("#cancelBtn");
+        this.author = document.querySelector("#author");
+        this.title = document.querySelector("#title");
+        this.pages = document.querySelector("#pages");
+        this.isRead = document.querySelector("#isRead");
+        this.newBook = document.querySelector(".new-book");
+        this.status_dialog = document.querySelector("#select-status")
 
-const library = [
-    {
-        title : "harry pooter",
-        author: "micheal",
-        pages: 300,
-        isRead: false
-    },
-    {
-        title : "journey to the west",
-        author: "choas incarnate",
-        pages: 300,
-        isRead: false
-    }, 
-    {
-        title : "kunfu panda",
-        author: "void general",
-        pages: 300,
-        isRead: false
-    },
-    {
-        title : "diablo",
-        author: "fantastic",
-        pages: 300,
-        isRead: false
+
     }
-];
-
-//Create a contructor function
-function Book(title ="",author = "",pages =0,isRead=false) {
-    this.title = title;
-    this.author = author;
-    this.isRead = isRead;
-    this.pages = pages;
-}
-
-//Create an interface to show the contents of the array
-//Display the books on the page: table or cards
-function displayBooks() {
-    book_section.replaceChildren();
-    library.forEach(book => {
+  render() {
+    this.book_section.replaceChildren();
+    this.library.forEach(book => {
       
           const article = document.createElement("article");
           const remove = document.createElement("button");
           remove.textContent = "remove"
           const changeStatus = document.createElement("button");
           changeStatus.textContent = "change status"
-          article.setAttribute("data-key",library.indexOf(book))
+          article.setAttribute("data-key",this.library.indexOf(book))
           const properties = document.createElement("ul");
       
           for(let prop in book) {
@@ -82,47 +76,56 @@ function displayBooks() {
           properties.appendChild(remove);
           properties.appendChild(changeStatus);
           article.appendChild(properties);
-          book_section.appendChild(article);
+          this.book_section.appendChild(article);
 
           remove.addEventListener("click",(e) => {
             let key = Number(e.target.parentElement.parentElement.getAttribute("data-key"));
-            library.splice(key,1);
-            displayBooks();
+            this.library.splice(key,1);
+            this.render();
     
           })
 
           changeStatus.addEventListener("click",(e) => {
             let key = Number(e.target.parentElement.parentElement.getAttribute("data-key"));
-            library[key].isRead = library[key].isRead === false ? true : false;
-            displayBooks()
+            this.library[key].isRead = this.library[key].isRead === false ? true : false;
+            this.render();
           })
+  })
+ }
+ bindEvents() {
+    this.newBook.addEventListener("click",() => {
+        this.dialog.showModal();
+    })
+    
+    this.dialog.addEventListener("close",(e) => {
+       this.render();
+    })
+    
+    this.confirmBtn.addEventListener("click",(e) => {
+        e.preventDefault();
+        const book = new Book(title.value,author.value,pages.value,isRead.value);
+        this.library.push(book);
+        this.dialog.close(); 
+    })
+    
+    this.cancelBtn.addEventListener("click",(e) => {
+        e.preventDefault();
+        this.dialog.close();
+    })
 
-         
-          
-          
-       })
-      
-       
+ }
 
 }
-displayBooks()
-//Add a new book button that allows users to input details of new book in a form
-newBook.addEventListener("click",() => {
-    dialog.showModal();
-})
 
-dialog.addEventListener("close",(e) => {
-    displayBooks();
-})
+class Book {
+    constructor(title = "",author="",pages=0,isRead=false) {
+        this.title = title;
+        this.author = author;
+        this.isRead = isRead;
+        this.pages = pages;
+    }
+}
 
-confirmBtn.addEventListener("click",(e) => {
-    e.preventDefault();
-    const book = new Book(title.value,author.value,pages.value,isRead.value);
-    library.push(book);
-    dialog.close(); 
-})
-
-cancelBtn.addEventListener("click",(e) => {
-    e.preventDefault();
-    dialog.close();
-})
+const libraryState = new State()
+libraryState.render();
+libraryState.bindEvents();
